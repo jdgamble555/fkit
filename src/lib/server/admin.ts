0ff25395d783e90a720bf2ebe180fddcc2c48b79
Jonathan Gamble <jdgamble555@gmail.com>
2023-07-04 12:47:30 -0500

    test

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -2,10 +2,10 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import { initializeApp, getApp, getApps } from 'firebase-admin/app';
-import pkg from 'firebase-admin';
+import { credential } from 'firebase-admin';
 
 const firebaseApp = getApps().length ? getApp() : initializeApp({
-    credential: pkg.credential.cert({
+    credential: credential.cert({
         projectId: FB_PROJECT_ID,
         clientEmail: FB_CLIENT_EMAIL,
         privateKey: FB_PRIVATE_KEY.replace(/\\n/g, '\n')
